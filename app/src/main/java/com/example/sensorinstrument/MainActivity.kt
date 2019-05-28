c5a16849dae9e953dcd30ed27b2abfc24fb734c2
Kamil Plich <kamilplich2@gmail.com>
2019-05-29 00:33:12 +0200

    proximity sensor should work

--- app/src/main/java/com/example/sensorinstrument/MainActivity.kt
+++ app/src/main/java/com/example/sensorinstrument/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.sensorinstrument
 
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,23 +11,36 @@ import android.widget.Button
 import android.widget.Switch
 import com.example.sensorinstrument.instruments.SineSynthesizer
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
 
     private var active = true
     private val synth: SineSynthesizer = SineSynthesizer()
     private lateinit var sensorManager: SensorManager
+
     private lateinit var rotationVectorSensor: Sensor
+    private lateinit var rotationVectorListener: SensorEventListener
+
+    private lateinit var proximitySensor: Sensor
+    private lateinit var proximitySensorListener: SensorEventListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
 
-        sensorManager.registerListener(RotationListener(windowManager, synth.getOscillatorFrequencyPort()),
+        rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+        rotationVectorListener = RotationListener(windowManager, synth.getOscillatorFrequencyPort())
+        sensorManager.registerListener(
+            rotationVectorListener,
             rotationVectorSensor,
             SensorManager.SENSOR_DELAY_FASTEST)
+
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        proximitySensorListener = ProximityListener(findViewById(R.id.synthActive))
+        sensorManager.registerListener(
+            proximitySensorListener,
+            proximitySensor,
+            SensorManager.SENSOR_DELAY_UI)
 
         findViewById<Button>(R.id.playingButton)!!.setOnTouchListener(PlayingButtonListener(synth))
 
@@ -44,6 +58,9 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         synth.stop()
+
+        sensorManager.unregisterListener(rotationVectorListener)
+        sensorManager.unregisterListener(proximitySensorListener)
     }
 
     override fun onResume() {
---
+++ app/src/main/java/com/example/sensorinstrument/ProximityListener.kt
@@ -0,0 +1,23 @@
+package com.example.sensorinstrument
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.widget.Switch
+
+class ProximityListener(private val switch: Switch): SensorEventListener {
+    companion object {
+        const val SENSOR_SENSITIVITY = 1.0
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event!!.sensor.type == Sensor.TYPE_PROXIMITY) {
+            if(event.values[0] < SENSOR_SENSITIVITY) {
+                switch.isChecked = !switch.isChecked
+                switch.callOnClick()
+            }
+        }
+    }
+}
