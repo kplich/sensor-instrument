b643a903ec52fe58645c06c581a5220af496b3a7
Kamil Plich <kamilplich2@gmail.com>
2019-05-30 00:13:13 +0200

    corrected proximity sensor logic

--- app/src/main/java/com/example/sensorinstrument/sensorListeners/ProximityListener.kt
+++ app/src/main/java/com/example/sensorinstrument/sensorListeners/ProximityListener.kt
@@ -10,13 +10,19 @@ class ProximityListener(private val switch: Switch): SensorEventListener {
         const val SENSOR_SENSITIVITY = 1.0
     }
 
+    private var wasClose: Boolean = false
+
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
 
     override fun onSensorChanged(event: SensorEvent?) {
         if (event!!.sensor.type == Sensor.TYPE_PROXIMITY) {
-            if(event.values[0] < SENSOR_SENSITIVITY) {
+            if(event.values[0] < SENSOR_SENSITIVITY && !wasClose) {
+                wasClose = true
                 switch.isChecked = !switch.isChecked
                 switch.callOnClick()
+            }
+            else {
+                wasClose = false
             }
         }
     }
